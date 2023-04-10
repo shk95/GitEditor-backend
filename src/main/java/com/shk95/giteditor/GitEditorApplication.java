f2be67254b09863c00b535360884207a091117c8
sh-kang <shk9510@gmail.com>
2023-04-10 19:33:13 +0900

    init

---
+++ src/main/java/com/shk95/giteditor/GitEditorApplication.java
@@ -0,0 +1,13 @@
+package com.shk95.giteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitEditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shk95/giteditor/GitEditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shk95.giteditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitEditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
