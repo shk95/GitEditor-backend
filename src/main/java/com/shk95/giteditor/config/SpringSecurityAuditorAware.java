d3f453389bc5f7b03757e43f8a0c0f5aebb1b9ce
sh-kang <shk9510@gmail.com>
2023-06-12 14:49:59 +0900

    fix: 임시 사용자를 위한 AuditAware 수정
    회원가입 등 권한이 없을경우.
    
    (cherry picked from commit a0f31ba29463951c0c10760de666de9d04ff5a54)

--- src/main/java/com/shk95/giteditor/config/SpringSecurityAuditorAware.java
+++ src/main/java/com/shk95/giteditor/config/SpringSecurityAuditorAware.java
@@ -1,23 +1,49 @@
 package com.shk95.giteditor.config;
 
+import com.shk95.giteditor.domain.common.constant.ProviderType;
+import com.shk95.giteditor.domain.common.security.Role;
 import com.shk95.giteditor.domain.model.user.CustomUserDetails;
 import com.shk95.giteditor.domain.model.user.UserId;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Collections;
 import java.util.Optional;
 
 @Slf4j
-public class SpringSecurityAuditorAware implements AuditorAware<UserId> {
+public class SpringSecurityAuditorAware implements AuditorAware<UserId> {// TODO:
 
 	@NotNull
 	@Override
 	public Optional<UserId> getCurrentAuditor() {
-		UserId currentUser = ((CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal())
-			.getUserEntityId();
-		log.info("Current Auditor called : {}", currentUser);
-		return Optional.of(currentUser);
+		CustomUserDetails currentUser =
+			Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+				.map(Authentication::getPrincipal)
+				.map(o -> {
+					if (o instanceof CustomUserDetails) {
+						CustomUserDetails customUserDetails = (CustomUserDetails) o;
+						log.info("Current Auditor called : {}", customUserDetails.getUserEntityId());
+						return customUserDetails;
+					}
+					return null;
+				}).orElseGet(() -> {
+					log.info("Current Auditor called : {}", "ANONYMOUS");
+					return CustomUserDetails.builder()
+						.userId("ANONYMOUS")
+						.defaultUsername("ANONYMOUS")
+						.providerType(ProviderType.ANONYMOUS)
+						.role(Role.ANONYMOUS)
+						.authorities(Collections.singletonList(new SimpleGrantedAuthority(Role.ANONYMOUS.getCode())))
+						.defaultEmail("ANONYMOUS")
+						.isUserEnabled(false)
+						.isUserEmailVerified(false).build();
+				});
+		UserId userId = currentUser.getUserEntityId();
+		log.info("Current Auditor called : {}", userId);
+		return Optional.ofNullable(userId);
 	}
 }
